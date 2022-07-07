45fd601958e31370a1b7ba077f93e558422f58ba
Ayomidemi <93412345+Ayomidemi@users.noreply.github.com>
2022-07-07 11:04:23 +0100

    7/7

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import { Todo } from '../model/model'
-import './styles.css'
-import TodoItem from './TodoItem'
+import React from "react";
+import { Todo } from "../model/model";
+import "./styles.css";
+import TodoItem from "./TodoItem";
 
 interface Props {
-  todos: Todo[]
-  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList : React.FC<Props> = ({todos, setTodos}) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
     <div className="container">
-       <div className='todos'>
-      {
-        todos.map(todo => (
-          <TodoItem todo={todo} key={todo.id} 
-          todos={todos}
-          setTodos={setTodos}
-          />
-        ))
-      }
-    </div>
-    </div>
-   
-  )
-}
+      <div className="todos">
+        <span className="todos__heading">Yet 2do</span>
 
-export default TodoList
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+
+      <div className="todos remove">
+        <span className="todos__heading">2dofied </span>
+
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
